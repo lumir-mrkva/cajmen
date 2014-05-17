92848629352a38377100582c1c534e91aefc45b5
Lumir Mrkva <lumir.mrkva@topmonks.com>
2014-05-17 03:06:07 +0200

    gitignore

--- server/publish.js
+++ server/publish.js
@@ -10,14 +10,21 @@ Meteor.publish('tables', function () {
 // Orders
 Orders = new Meteor.Collection('orders');
 
-// Publish all items for requested list_id.
 Meteor.publish('orders', function (table_id) {
   check(table_id, String);
   return Orders.find({table_id: table_id});
 });
 
+//Items
 Items = new Meteor.Collection('items');
 
 Meteor.publish('items', function () {
   return Items.find({});
+});
+
+//OrderedItems
+OrderedItems = new Meteor.Collection('orderedItems');
+
+Meteor.publish('orderedItems', function(order_id) {
+    return OrderedItems.find({order_id: order_id});
 });
